3e40a6852db3c3a919aa5c78a288a252df75fd7a
loli9024 <54463078+loli9024@users.noreply.github.com>
2020-10-05 09:47:14 +1000

    Add files via upload

---
+++ application/views/2.php
@@ -0,0 +1,18 @@
+
+<div class="container">
+	<div class="card w-75">
+		<div class="card-body">
+			<h5 class="card-title">Car 2</h5>
+			<p class="card-text"><?php if(isset($hidden_text)) echo $hidden_text; ?></p>
+			<pre>
+                  .
+    __            |\
+ __/__\___________| \_
+|   ___    |  ,|   ___`-.
+|  /   \   |___/  /   \  `-.
+|_| (O) |________| (O) |____|
+   \___/          \___/
+		</pre>
+		</div>
+	</div>
+</div>
