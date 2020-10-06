24b12854d2162e8cc896c47f0a3826063d40131c
loli9024 <54463078+loli9024@users.noreply.github.com>
2020-10-06 11:54:36 +1000

    Update database.php

--- videoPlay/application/config/database.php
+++ videoPlay/application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'mysql',
+	'hostname' => '35.184.84.202',
 	'username' => 'root',
 	'password' => 'secret',
 	'database' => 'YouTube',
